43dcd2b809fa085bf30072c962ee1014757fd4c3
Nikita Merzlyakov <merzlyakov02@gmail.com>
2021-09-24 17:17:49 +0300

    edited RandomUtil

--- src/ru/vsu/cs/util/RandomUtil.java
+++ src/ru/vsu/cs/util/RandomUtil.java
@@ -3,8 +3,9 @@ package ru.vsu.cs.util;
 import java.util.Random;
 
 public class RandomUtil {
+    private static final Random rnd = new Random();
+
     public static int randInt(int i1, int i2) {
-        Random rnd = new Random();
         return rnd.nextInt(i2 - i1) + i1;
     }
 }
